a1b9c5e3ce15a75b5a6050505bc84c1adca793d1
RSW <rswpub@gmail.com>
2022-07-24 20:55:55 -0600

    Handle state for the children in the parent

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ export default function App(props) {
   const [boxArray, setBoxArray] = React.useState(boxData)
 
   const boxList = boxArray.map(function (boxObject) {
-    return <Box id={boxObject.id} isOn={boxObject.on} />
+    return <Box key={boxObject.id} id={boxObject.id} isOn={boxObject.on} toggle={toggle} />
   })
+
+  function toggle(boxId) {
+    setBoxArray(function (prevBoxArray) {
+      return [...prevBoxArray.slice(0, boxId - 1), {id: boxId, on: !prevBoxArray[boxId - 1].on}, ...prevBoxArray.slice(boxId)]
+    })
+  }
 
   return (
     <div style={{ backgroundColor: props.darkMode ? "#222" : "#ccc" }}>
--- src/Box.js
+++ src/Box.js
@@ -2,16 +2,8 @@ import React from "react"
 
 export default function Box(props) {
 
-    const [isOn, setIsOn] = React.useState(props.isOn)
-
-    function handleClick() {
-        setIsOn(function (prevIsOn) {
-            return !prevIsOn
-        })
-    }
-
     return (
-        <div id={props.id} key={props.id} onClick={handleClick} className={"box " + (isOn ? "on" : "off")}>
+        <div id={props.id} onClick={function () { return props.toggle(props.id) }} className={"box " + (props.isOn ? "on" : "off")}>
             {`Box ${props.id}`}
         </div>
     )
