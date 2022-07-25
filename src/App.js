8e3d8d450e90275d45bc065b83ddfb667f73bcae
RSW <rswpub@gmail.com>
2022-07-24 19:27:45 -0600

    Display boxes plus some dynamic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-export default function App() {
+import React from 'react';
+import boxData from './boxes'
+
+export default function App(props) {
+  const [boxArray, setBoxArray] = React.useState(boxData)
+
+  const boxList = boxArray.map(function (boxObject) {
+    return <div id={boxObject.id} key={boxObject.id} onClick={handleClick} className={"box " + (boxObject.on ? "on" : "off")}>{ `Box ${boxObject.id}`}</div>;
+  })
+
+  function handleClick() {
+    const clickedBox = 
+    setBoxArray(function (prevBoxArray) {
+      return 
+    })
+  }
+
   return (
-    <div>
+    <div style={{ backgroundColor: props.darkMode ? "#222" : "#ccc" }}>
+      {boxList}
     </div>
   );
 }
